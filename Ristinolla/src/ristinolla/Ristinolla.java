376f475fa23be64fe20fdcbd2a9877d1145341a1
Mikko Översti <mikko.oversti@helsinki.fi>
2012-09-04 12:35:54 +0300

    gitignore add

--- Ristinolla/src/ristinolla/Ristinolla.java
+++ Ristinolla/src/ristinolla/Ristinolla.java
@@ -1,11 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package ristinolla;
 
 /**
- *
+ * Ristinollapeli.
  * @author moversti
  */
 public class Ristinolla {
