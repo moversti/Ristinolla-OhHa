d63f6f72e52caba234cadd8d9d35206f70236235
Mikko Översti <mikko.oversti@helsinki.fi>
2012-09-04 12:58:40 +0300

    jotai tehty

--- Ristinolla/src/ristinolla/Ristinolla.java
+++ Ristinolla/src/ristinolla/Ristinolla.java
@@ -12,6 +12,8 @@ public class Ristinolla {
     public static void main(String[] args) {
         Pelilauta pelilauta = new Pelilauta();
         pelilauta.muutaRuutu(3, 4, Ruutu.X);
+        pelilauta.muutaRuutu(4, 5, Ruutu.X);
+        pelilauta.muutaRuutu(5, 6, Ruutu.O);
         System.out.println(pelilauta);
     }
 }
