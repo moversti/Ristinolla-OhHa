efa023670c789302eb22fe89b794b449db437d76
Mikko Översti <mikko.oversti@helsinki.fi>
2012-09-04 12:55:01 +0300

    jotai tehty

--- Ristinolla/src/ristinolla/Ristinolla.java
+++ Ristinolla/src/ristinolla/Ristinolla.java
@@ -10,6 +10,8 @@ public class Ristinolla {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pelilauta pelilauta = new Pelilauta();
+        pelilauta.muutaRuutu(3, 4, Ruutu.X);
+        System.out.println(pelilauta);
     }
 }
