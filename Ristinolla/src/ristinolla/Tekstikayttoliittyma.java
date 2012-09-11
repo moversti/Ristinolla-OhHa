dc7bf6247ebd0f4370cffb885f9da47fa7424d32
Mikko Översti <mikko.oversti@helsinki.fi>
2012-09-12 00:06:30 +0300

    menussa pelaa vaihtoehto

--- Ristinolla/src/ristinolla/Tekstikayttoliittyma.java
+++ Ristinolla/src/ristinolla/Tekstikayttoliittyma.java
@@ -25,6 +25,7 @@ public class Tekstikayttoliittyma implements Kayttoliittyma {
         this.pelilauta = pelilauta;
         this.voitontestaaja = new Voitontestaaja(pelilauta, rivinPituus);
         lukija = new Scanner(System.in);
+        voittaja = Ruutu.O;
     }
 
     /**
@@ -55,24 +56,39 @@ public class Tekstikayttoliittyma implements Kayttoliittyma {
      * Kysyy komennon ja kutsuu metodeita tekemään hommia.
      */
     private void kysyKomento() {
-        System.out.print("Komento[X/O/Tallenna/Lataa/Undo]: ");
+        System.out.print("Komento[Pelaa/X/O/Tallenna/Lataa/Undo]: ");
         String syote = lukija.nextLine().toUpperCase();
-        if (syote.equals("X") || syote.equals("O")) {
-            System.out.print("Koordinaatti x: ");
-            String xkoord = lukija.nextLine();
-            System.out.print("Koordinaatti y: ");
-            String ykoord = lukija.nextLine();
-            Ruutu ruutu;
-            if (syote.equals("X")) {
-                ruutu = Ruutu.X;
-            } else {
-                ruutu = Ruutu.O;
-            }
-            int xkoo = Integer.parseInt(xkoord);
-            int ykoo = Integer.parseInt(ykoord);
-            pelilauta.muutaRuutu(xkoo, ykoo, ruutu);
-            voittaja = ruutu;
-            pelataan = !voitontestaaja.testaa();
+        if (syote.equals("X") || syote.equals("O") || syote.equals("P")) {
+            do {
+                System.out.print("Koordinaatti x: ");
+                String a = lukija.nextLine();
+                if (a.isEmpty()) {
+                    break;
+                }
+                String xkoord = a;
+                System.out.print("Koordinaatti y: ");
+                a = lukija.nextLine();
+                if (a.isEmpty()) {
+                    break;
+                }
+                String ykoord = a;
+                Ruutu ruutu;
+                if (syote.equals("X")) {
+                    ruutu = Ruutu.X;
+                } else if (syote.equals("O")) {
+                    ruutu = Ruutu.O;
+                } else if (voittaja == Ruutu.O) {
+                    ruutu = Ruutu.X;
+                } else {
+                    ruutu = Ruutu.O;
+                }
+                int xkoo = Integer.parseInt(xkoord);
+                int ykoo = Integer.parseInt(ykoord);
+                pelilauta.muutaRuutu(xkoo, ykoo, ruutu);
+                voittaja = ruutu;
+                pelataan = !voitontestaaja.testaa();
+                printtaaLauta();
+            } while (syote.equals("P")&&pelataan);
         } else if (syote.equals("U")) {
             pelilauta.undo();
         } else {
